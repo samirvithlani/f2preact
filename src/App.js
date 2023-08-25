30862c922a72d5426430e99b3536c77b518b9765
samirvithlani <samir.vithlani83955@gmail.com>
2023-08-25 19:36:17 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+//js.....
+//every tag must be closed
+//only one root element
+var a =100;
+var name ="royal"
+var isActive =false
+var user ={
+  name:"raj",
+  age:20,
+}
+var students = [
+  {
+    id:1,
+    name:"amit",
+  },
+  {
+    id:2,
+    name:"ram",
+  }
+
+]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO</h1>
+      {a}
+      <h1>{a}</h1>
+      <h2>Institute Name = {name}</h2>
+      <h3>STATUS = {isActive == true ? "Active" :"Not Active"}</h3>
+      <h2>
+        User Name ={user.name}
+      </h2>
+      <h3>Age = {user.age}</h3>
+      {
+        students.map((stu)=>{
+          return(<div>
+            <h1>Student Name = {stu.name}</h1>
+            <h2>Student Id = {stu.id}</h2>
+          </div>)
+        })
+      }
+
     </div>
   );
 }
